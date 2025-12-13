44b8a74bad7d7daafb3b7ed0ebcbc93e8075186c
Mateus Tavares <mateustav@gmail.com>
2025-12-12 23:43:02 -0700

    Include knex

---
+++ knexfile.ts
@@ -0,0 +1,62 @@
+import "dotenv/config";
+import type { Knex } from "knex";
+
+// Update with your config settings.
+
+const config: { [key: string]: Knex.Config } = {
+  development: {
+    client: "postgresql",
+    connection: {
+      database: process.env.PGDATABASE!,
+      user: process.env.PGUSER!,
+      password: process.env.PGPASSWORD!,
+      host: process.env.PGHOST!,
+      port: parseInt(process.env.PGPORT!),
+    },
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      tableName: "knex_migrations",
+    },
+  },
+
+  staging: {
+    client: "postgresql",
+    connection: {
+      database: process.env.PGDATABASE!,
+      user: process.env.PGUSER!,
+      password: process.env.PGPASSWORD!,
+      host: process.env.PGHOST!,
+      port: parseInt(process.env.PGPORT!),
+    },
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      tableName: "knex_migrations",
+    },
+  },
+
+  production: {
+    client: "postgresql",
+    connection: {
+      database: process.env.PGDATABASE!,
+      user: process.env.PGUSER!,
+      password: process.env.PGPASSWORD!,
+      host: process.env.PGHOST!,
+      port: parseInt(process.env.PGPORT!),
+    },
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      tableName: "knex_migrations",
+    },
+  },
+};
+
+export default config;
